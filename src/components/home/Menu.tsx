3904621ce04701782069940ba05f2ab283acb06d
andrealessi89 <andrealessi89@gmail.com>
2025-04-16 16:03:08 -0300

    lupa

--- src/components/home/Menu.tsx
+++ src/components/home/Menu.tsx
@@ -52,18 +52,20 @@ function MenuIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width={24}
+      height={24}
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth={2}
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <line x1="4" x2="20" y1="12" y2="12" />
-      <line x1="4" x2="20" y1="6" y2="6" />
-      <line x1="4" x2="20" y1="18" y2="18" />
+      {/* lente */}
+      <circle cx={11} cy={11} r={8} />
+      {/* cabo/manípulo */}
+      <line x1={21} y1={21} x2={16.65} y2={16.65} />
     </svg>
   );
 }
+
